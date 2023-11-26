2f1322dcccf6182ac7fc6dc8d476b0169546fdb6
Oleg Musatov <olegmusatov97@gmail.com>
2023-11-26 16:08:42 +0700

    Add start with docker

--- backend/stripe_service_backend/settings.py
+++ backend/stripe_service_backend/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'stripe_service_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
